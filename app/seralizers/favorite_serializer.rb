bb39b945022740f2e96e3f923b1a124890c5ca7e
Anya <anya.s.litvinova@gmail.com>
2020-04-07 18:48:24 -0400

    deployment err

--- app/seralizers/favorite_serializer.rb
+++ app/seralizers/favorite_serializer.rb
@@ -2,5 +2,4 @@ class FavoriteSerializer < ActiveModel::Serializer
   attributes :id, :user_id, :place_id
   belongs_to :user
   belongs_to :place, :class_name => "Business"
-  validates_uniqueness_of :user_id, :scope => :place_id
 end
